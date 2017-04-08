77045bb3a81241fa377e9de01b328961a5700717
Ekaterina Demina <katedisenko@mail.ru>
2017-04-08 12:05:34 +0800

    #11 Личный проект: подробности

--- js/pictures.js
+++ js/pictures.js
@@ -12,9 +12,21 @@ var pictureTemplate = document.querySelector('#picture-template').content;
 
 var picturesBlock = document.querySelector('.pictures');
 
-var uploadForm = document.querySelector('.upload-overlay');
+var uploadOverlay = document.querySelector('.upload-overlay');
 
 var gallery = document.querySelector('.gallery-overlay');
+
+var galleryCloseBtn = gallery.querySelector('.gallery-overlay-close');
+
+var uploadForm = document.querySelector('.upload-image');
+
+var uploadFileName = uploadForm.querySelector('.upload-input');
+
+var uploadCancelBtn = uploadOverlay.querySelector('.upload-form-cancel');
+
+var uploadSubmitBtn = uploadOverlay.querySelector('.upload-form-submit');
+
+var uploadComment = uploadOverlay.querySelector('.upload-form-description');
 
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -73,7 +85,119 @@ var fillGallery = function (photo) {
   gallery.querySelector('.comments-count').textContent = photo.comment.length;
 };
 
+function findPhoto(array, url) {
+  var i = 0;
+  var result = -1;
+  while ((i < array.length) && (result < 0)) {
+    if (url === array[i].url) {
+      result = i;
+    }
+    i++;
+  }
+  if (result > -1) {
+    return photos[result];
+  } else {
+    return false;
+  }
+}
+
+var onPictureEscPress = function (evt) {
+  if (evt.keyCode === 27) {
+    closePicture();
+  }
+};
+
+var closePicture = function () {
+  gallery.classList.add('invisible');
+  document.removeEventListener('keydown', onPictureEscPress);
+};
+
+var openPicture = function (evt) {
+  if (evt.currentTarget.className === 'picture') {
+    var picture = evt.currentTarget;
+    var pictureObj = findPhoto(photos, picture.querySelector('img').getAttribute('src'));
+    if (typeof pictureObj === 'object') {
+      fillGallery(pictureObj);
+      gallery.classList.remove('invisible');
+    }
+  }
+  document.addEventListener('keydown', onPictureEscPress);
+};
+
+var onUploadOverlayEscPress = function (evt) {
+  if (evt.keyCode === 27) {
+    closeUploadOverlay();
+  }
+};
+
+var closeUploadOverlay = function () {
+  uploadOverlay.classList.add('invisible');
+  uploadForm.classList.remove('invisible');
+  uploadFileName.value = '';
+  document.removeEventListener('keydown', onUploadOverlayEscPress);
+};
+
+var openUploadOverlay = function () {
+  uploadForm.classList.add('invisible');
+  uploadOverlay.classList.remove('invisible');
+  document.addEventListener('keydown', onUploadOverlayEscPress);
+};
+
 photos = fillBlockPictures();
-uploadForm.classList.add('invisible');
-gallery.classList.remove('invisible');
-fillGallery(photos[0]);
+uploadOverlay.classList.add('invisible');
+uploadForm.classList.remove('invisible');
+
+var pictures = picturesBlock.querySelectorAll('.picture');
+for (var i = 0; i < pictures.length; i++) {
+  pictures[i].addEventListener('click', function (evt) {
+    evt.preventDefault();
+    openPicture(evt);
+  });
+  pictures[i].addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 13) {
+      openPicture(evt);
+    }
+  });
+}
+
+galleryCloseBtn.addEventListener('click', function () {
+  closePicture();
+});
+
+galleryCloseBtn.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    closePicture();
+  }
+});
+
+uploadFileName.addEventListener('change', function (evt) {
+  openUploadOverlay(evt);
+});
+
+uploadCancelBtn.addEventListener('click', function () {
+  closeUploadOverlay();
+});
+
+uploadCancelBtn.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    closeUploadOverlay();
+  }
+});
+
+uploadComment.addEventListener('focus', function () {
+  document.removeEventListener('keydown', onUploadOverlayEscPress);
+});
+
+uploadComment.addEventListener('blur', function () {
+  document.addEventListener('keydown', onUploadOverlayEscPress);
+});
+
+uploadSubmitBtn.addEventListener('click', function () {
+  closeUploadOverlay();
+});
+
+uploadSubmitBtn.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    closeUploadOverlay();
+  }
+});
