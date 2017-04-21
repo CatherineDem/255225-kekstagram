79453a640e4c50e2b74f60e6a7d209dd3d97fbbf
Ekaterina Demina <katedisenko@mail.ru>
2017-04-21 12:01:17 +0800

    Правка модуля error-handler для Edge

--- js/error-handler.js
+++ js/error-handler.js
@@ -5,5 +5,5 @@ window.errorHandler = (function (id, element, errmess) {
   var errMessageElement = errMessageTemplate.cloneNode(true);
   errMessageElement.querySelector('.err-message').id = id;
   errMessageElement.querySelector('.err-message').textContent = errmess;
-  element.insertAdjacentElement('afterEnd', errMessageElement.firstElementChild);
+  element.insertAdjacentElement('afterend', errMessageElement.querySelector('.err-message'));
 });
